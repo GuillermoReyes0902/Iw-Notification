89a2c1537cdf0d4c7534a4b29af46f556befd212
Yareni Grajales <yareni@iw.digital>
2025-08-12 14:36:22 -0600

    Firebase pruebas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:iwproject/domain/models/user_model.dart';
 import 'package:iwproject/firebase_options.dart';
@@ -18,6 +19,15 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   if (Platform.isMacOS) {
+    //
+    FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+    var token = await firebaseMessaging.getToken();
+    print(token);
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('[onMessage] message: $message');
+    });
+    FirebaseMessaging.onBackgroundMessage(onBackgroundMessage);
+    //
     _launchAtStartupInit();
   }
 
@@ -30,6 +40,11 @@ void main() async {
       child: const MyApp(),
     ),
   );
+}
+
+Future<void> onBackgroundMessage(RemoteMessage message) {
+  print('[onBackgroundMessage] message: $message');
+  return Future.value();
 }
 
 _launchAtStartupInit() async {
